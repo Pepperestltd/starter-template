72ab47a06a0ed348c88431fba99d3765251619a1
Chime Chinonso <66419613+Nasglobal@users.noreply.github.com>
2024-08-12 00:03:05 +0100

    my first commit

---
+++ app/(themeName)/pages/about/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    <p className="font-bold text-4xl text-purple-900">
+      About page
+    </p>
+   
+  
+</main>
+  )
+}
+
+export default page
---
+++ app/(themeName)/pages/contact/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    <p className="font-bold text-4xl text-purple-900">
+      Contact us page
+    </p>
+   
+  
+</main>
+  )
+}
+
+export default page
