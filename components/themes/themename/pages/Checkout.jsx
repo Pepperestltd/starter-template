5e93a4bda0dd6d1bf1b9dda33d9912464507a83f
Chime Chinonso <66419613+Nasglobal@users.noreply.github.com>
2024-09-18 12:48:31 +0100

    adding checkout and subscription product and few modifications

--- components/themes/themename/pages/Checkout.jsx
+++ components/themes/themename/pages/Checkout.jsx
@@ -1,31 +1,28 @@
+"use client"
 import React from "react";
-import CheckoutForm from "../forms/CheckoutForm";
-import ServiceCheckoutForm from "../forms/ServiceCheckoutForm";
-import DigitalProductCheckoutForm from "../forms/DigitalProductCheckoutForm"
-import SubscriptionCheckoutForm from "../forms/SubscriptionCheckoutForm";
 import { useSearchParams } from "next/navigation";
 
 const CheckoutDetail = ({ setStep, step }) => {
   const searchParams = useSearchParams();
   const checkoutType = searchParams.get("type");
   return (
-    <div className="">
+    <div className="flex justify-center">
       {checkoutType == "products" ? (
-        <p>Render physical product checkout form here</p>
+        <p className="text-center mt-20 text-purple-800">Render physical product checkout form here</p>
       ) :
        
       (
         checkoutType == "digital-products" ? 
         
-        <p>Render digital product checkout form here</p>
+        <p className="text-center mt-20 text-purple-800">Render digital product checkout form here</p>
       :
       (
         checkoutType == "services" ? 
         
-        <p>Render services checkout form here</p>
+        <p className="text-center mt-20 text-purple-800">Render services checkout form here</p>
         :
 
-        <p>Render subscription product checkout form here</p>
+        <p className="text-center mt-20 text-purple-800" >Render subscription product checkout form here</p>
         
       ) 
       )
